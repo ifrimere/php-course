6b0eab1ff814a6a36cd957df69cd7bced10cf999
ifrimere <ifrimere@acclaro.com>
2020-04-20 13:10:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	    echo "<h1>Welcome to ".$name."!</h1>";
 	    echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-	    $products['Computer']=750;
+		  $products['Computer']=750;
 	    $products['Car']=15000;
 	    $products['iPhone']=1000;
 	    $products['Toaster']=75;
@@ -20,6 +20,13 @@
 		    echo "<p>The ".$key." costs ".$value."</p>";
 	    }
 
+	    echo "<h2>Items you can afford</h2>";
+
+	    foreach($products as $key => $value){
+		    if($value <= $credit ){
+		    	echo "<p>".$key."</p>"; 
+		    }
+	    }
     ?>
   </body>
 </html>
